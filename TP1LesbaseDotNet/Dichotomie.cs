e1444760e714ed6ad7676c7934a023acb716f8bc
mohamed el haddadi <77499801+Medhaddadi@users.noreply.github.com>
2023-03-23 11:20:41 +0000

    Add project files.

---
+++ TP1LesbaseDotNet/Dichotomie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1LesbaseDotNet
+{
+    internal class Dichotomie : Function
+    {
+        private double a;
+        private double b;
+        private double prec;
+        public Dichotomie() {
+
+        }
+
+        public double computeFunctionValue(double d)
+        {
+            /*  return Math.Pow(d, 5) + 3 * Math.Pow(d, 4) + 2 * Math.Pow(d, 3) + Math.Pow(d, 2) + 3 * d + 1;*/
+            return Math.Pow(d, 3) - 3 * d + 1;
+        }
+
+        public  void solveDichotomie (double a, double b , double prec )
+        {
+            while ((b-a)>prec)
+            {
+                double c = (a + b) / 2;
+                if (computeFunctionValue(a)*computeFunctionValue(c)<=0)
+                {
+                   b= c;
+                }else
+                    a=c;
+
+            }
+            this.a = a;
+            this.b = b;
+            this.prec = prec;
+        }
+        public void afficher()
+        {
+            Console.WriteLine("a = " + this.a + " b = " + b);
+        }
+
+    }
+}
---
+++ TP1LesbaseDotNet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1LesbaseDotNet
+{
+    class Program 
+    {
+       public static void Main(string[] args)
+        {
+            Dichotomie dichotomie = new Dichotomie();
+            dichotomie.solveDichotomie(-1000, 1000, 1 * Math.Exp(-15));
+          /*  dichotomie.solveDichotomie(-2.5, 2,-0.25); */
+            Console.WriteLine("hi");
+            dichotomie.afficher(); 
+                
+        }
+
+       
+    }
+}
